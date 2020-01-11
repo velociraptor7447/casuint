b0e433ad76bd315ae5299bbde6299326e4c56996
Komi Abotsi <abotsi.n2@gmail.com>
2020-01-11 15:33:00 -0600

    CASU011120-1 Fixing menu CSS  after adding new menu items.

--- themes/custom/casu_bootstrap/js/contact.js
+++ themes/custom/casu_bootstrap/js/contact.js
@@ -3,7 +3,7 @@
     attach: function (context, settings) {
       $(document).ready(function(){
         $('#contact-message-contactez-nous-form .form-item-name label').text('Nom et Prénom');
-        $('#contact-message-contactez-nous-form .form-item-mail label').text('Adresse Electronique(Email)');
+        $('#contact-message-contactez-nous-form .form-item-mail label').text('Adresse Electronique (Email)');
         $('#contact-message-contactez-nous-form .form-item-subject-0-value label').text('Sujet');
         $('#contact-message-contactez-nous-form .form-item-message-0-value label').text('Message');
         $('#contact-message-contactez-nous-form button#edit-submit').text('Envoyez');
