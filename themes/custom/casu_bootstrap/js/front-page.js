a939d9f8ce4f21787e18cbd781359739f585de52
Komi Abotsi <abotsi.n2@gmail.com>
2020-01-14 00:55:09 -0600

    CASU-011420-1 First config export, and making icons look better.

--- themes/custom/casu_bootstrap/js/front-page.js
+++ themes/custom/casu_bootstrap/js/front-page.js
@@ -5,14 +5,17 @@
         $('.region.region-navigation .logo img').attr('src', '/themes/custom/casu_bootstrap/logo.jpg');
 
         // Get the hrefs
-        let href_bureau = $('#block-views-block-bureau-block-1 .views-field.views-field-name a').attr('href');
-        let href_projets = $('#block-views-block-projets-block-1 .views-field.views-field-name a').attr('href');
-        let href_contact = $('#block-views-block-contact-block-1 .views-field.views-field-name a').attr('href');
+        $('#block-views-block-bureau-block-1 .views-col.col-1').css('width', '100%');
+        $('#block-views-block-projets-block-1 .views-col.col-1').css('width', '100%');
+        $('#block-views-block-contact-block-1 .views-col.col-1').css('width', '100%');
+
+
 
         // Adding some divs to each section
-        $('#block-views-block-bureau-block-1').wrap("<div class='casu-icon' id='casu-bureau'></a></div>");
-        $('#block-views-block-projets-block-1').wrap("<div class='casu-icon' id='casu-projets'></a></div>");
-        $('#block-views-block-contact-block-1').wrap("<div class='casu-icon' id='casu-contact'></a></div>");
+      //  $('#block-views-block-bureau-block-1 span a').append("<i class='fa fa-file-text-o fa-3x'></i>");
+        //$('#block-views-block-bureau-block-1').wrap("<div class='casu-icon' id='casu-bureau'></a></div>");
+        //$('#block-views-block-projets-block-1').wrap("<div class='casu-icon' id='casu-projets'></a></div>");
+        //$('#block-views-block-contact-block-1').wrap("<div class='casu-icon' id='casu-contact'></a></div>");
       });
     }
   };
