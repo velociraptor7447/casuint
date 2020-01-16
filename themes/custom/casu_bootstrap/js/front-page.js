e52b9ab877c562272afef0e7e2e991a7cfc407f6
Komi Abotsi <abotsi.n2@gmail.com>
2020-01-15 23:58:55 -0600

    CASU-011520-1 A propos and mission page. Fixed tablette view for navigation menu

--- themes/custom/casu_bootstrap/js/front-page.js
+++ themes/custom/casu_bootstrap/js/front-page.js
@@ -4,18 +4,13 @@
       $(document).ready(function(){
         $('.region.region-navigation .logo img').attr('src', '/themes/custom/casu_bootstrap/logo.jpg');
 
-        // Get the hrefs
+        // This is a hack
         $('#block-views-block-bureau-block-1 .views-col.col-1').css('width', '100%');
         $('#block-views-block-projets-block-1 .views-col.col-1').css('width', '100%');
         $('#block-views-block-contact-block-1 .views-col.col-1').css('width', '100%');
 
-
-
-        // Adding some divs to each section
-      //  $('#block-views-block-bureau-block-1 span a').append("<i class='fa fa-file-text-o fa-3x'></i>");
-        //$('#block-views-block-bureau-block-1').wrap("<div class='casu-icon' id='casu-bureau'></a></div>");
-        //$('#block-views-block-projets-block-1').wrap("<div class='casu-icon' id='casu-projets'></a></div>");
-        //$('#block-views-block-contact-block-1').wrap("<div class='casu-icon' id='casu-contact'></a></div>");
+        // This is a hack
+        $('.view-projets-full-view > .view-content > .table-responsive > .table').removeClass('table').addClass('table-casu');;
       });
     }
   };
